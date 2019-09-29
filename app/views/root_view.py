850c16b57190d1d5b55f686aa4d78fa291235bec
Ushinji <siguma971@gmail.com>
2019-09-29 10:53:06 +0900

    Add i18n to root page.

--- app/views/root_view.py
+++ app/views/root_view.py
@@ -1,7 +1,11 @@
 from flask import render_template
 from app import application
+from app.utils.i18n import I18n
+
+i18n = I18n('translate')
 
 
 @application.route('/', methods=['GET'])
 def root_index():
-    return render_template('root/index.html')
+    i18n.set_locale('jp')
+    return render_template('root/index.html', i18n=i18n)
